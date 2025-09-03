64277aea4ce2621f404ffdf6af0da0015e4cb642
ToinonMathis <toinon.math+github@gmail.com>
2025-09-04 00:33:10 +0200

    responsive recipe list

--- resources/js/pages/Recipe.vue
+++ resources/js/pages/Recipe.vue
@@ -3,19 +3,18 @@
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <div class="flex flex-row justify-center gap-20 mb-5">
+            <div class="flex flex-row justify-center gap-4 gap-md-20 mb-5">
                 <h1>Tous</h1>
                 <h1>Les entrées</h1>
                 <h1>Les plats</h1>
                 <h1>Les dessert</h1>
             </div>
             <div class="flex flex-wrap -m-2">
-                <div class="p-2 w-1/4 sm:w-1/2 md:w-1/3 lg:w-1/4" v-for="n in 8"
+                <div class="p-2 w-1/1 sm:w-1/2 md:w-1/3 lg:w-1/4" v-for="n in 8"
                      :key="n">
                     <CardRecipe/>
                 </div>
             </div>
-            {{ recipes }}
         </div>
     </AppLayout>
 </template>
